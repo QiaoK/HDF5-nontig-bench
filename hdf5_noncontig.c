91825896f441ffc04d1a7b58cc71f4e917bbc40b
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-04-25 23:37:23 -0500

    Update hdf5_noncontig.c

--- hdf5_noncontig.c
+++ hdf5_noncontig.c
@@ -383,7 +383,7 @@ int main (int argc, char **argv) {
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &nprocs);
 
-    while ((i = getopt (argc, argv, "d:r:n:c:")) != EOF) switch (i) {
+    while ((i = getopt (argc, argv, "d:s:n:c:")) != EOF) switch (i) {
         case 'c': {
             req_count = atoi(optarg);
             break;
